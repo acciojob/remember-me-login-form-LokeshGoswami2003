f7227c47fd8d292ea2ec2b87937a17236b0c688e
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-10-30 16:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener('click', () => {
+	const username = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
+	const rememberme = document.getElementById('checkbox');
+	if(rememberme.checked) {
+		localStorage.setItem("username", username);
+		localStorage.setItem("password", password);
+	} else {
+		localStorage.clear();
+	}
+	alert(`Logged in as ${username}`);
+});
+
+document.getElementById('existing').addEventListener('click', () => {
+	const username = localStorage.getItem("username");
+	alert(`Logged in as ${username}`);
+})
+
+function initialFunction() {
+	if(localStorage.getItem("username") != null)
+	document.getElementById('existing').style.display = "block";
+}
+
+initialFunction();
